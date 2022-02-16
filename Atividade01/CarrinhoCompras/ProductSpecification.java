e1bed7ba1892d79f3ffe5999fe41022feb5ac8d1
Davi Camara <davirego9.dc@gmail.com>
2022-02-16 14:11:29 -0300

    Reestruturação do pacote do git

---
+++ Atividade01/CarrinhoCompras/ProductSpecification.java
@@ -0,0 +1,39 @@
+package CarrinhoCompras;
+
+public class ProductSpecification {
+
+	private String description;
+	private double price;
+	private String itemID;
+
+	public ProductSpecification(String description, double price, String itemID) {
+		this.description = description;
+		this.price = price;
+		this.itemID = itemID;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getItemID() {
+		return itemID;
+	}
+
+	public void setItemID(String itemID) {
+		this.itemID = itemID;
+	}
+
+}
---
+++ Atividade01/CarrinhoCompras/SaleLineItem.java
@@ -0,0 +1,19 @@
+package CarrinhoCompras;
+
+public class SaleLineItem {
+
+	private int quantity;
+	ProductSpecification product;
+
+	public SaleLineItem(ProductSpecification product, int quantity) {
+		this.quantity = quantity;
+		this.product = product;
+	}
+
+	public double getSubTotal() {
+		double subtotal = quantity * product.getPrice();
+
+		return subtotal;
+	}
+
+}
