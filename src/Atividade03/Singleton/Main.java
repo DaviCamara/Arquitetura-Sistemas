4c21df7356982b229a936bdef991ffa360aae96d
Davi Camara <davirego9.dc@gmail.com>
2022-03-15 19:23:19 -0300

    instanciamento de singleton via variável, acesso de métodos via referência de variavel, prova de conceito de conect e disconnect com banco de dados

--- src/Atividade03/Singleton/Main.java
+++ src/Atividade03/Singleton/Main.java
@@ -3,12 +3,15 @@ package Atividade03.Singleton;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		AcessoDBSingleton.getInstance();
-		System.out.println("------------------------------------------------------------");
-		AcessoDBSingleton.getInstance().disconnect();
-		System.out.println("------------------------------------------------------------");
-		AcessoDBSingleton.getInstance();
+		for (int i = 0; i < 2; i++) {
+
+			AcessoDBSingleton AcessoDB = AcessoDBSingleton.getInstance();
+			System.out.println("------------------------------------------------------------");
+			AcessoDB.isConnected();
+			AcessoDB.disconnect();
+			AcessoDB.isConnected();
+			System.out.println("------------------------------------------------------------");
+		}
 	}
 
 }
