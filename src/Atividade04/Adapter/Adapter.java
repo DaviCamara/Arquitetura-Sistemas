2fbe59d6fb8fa2c8d2b123030949fdcc21a64b16
davicamara <davicamara@unimedceara.com.br>
2022-03-29 19:42:57 -0300

    Correção de nome do pacote/classes

---
+++ src/Atividade04/Adapter/Adapter.java
@@ -0,0 +1,10 @@
+package Atividade04.Adapter;
+
+
+//Interface do Adapter 
+public interface Adapter {
+    
+	
+	// Retorno da conversão de velocidade
+	double getSpeed();
+}
--- src/Atividade04/Adaptor/Ferrari.java
+++
@@ -1,9 +0,0 @@
-package Atividade04.Adaptor;
-
-public class Ferrari implements Interface {
-	 
-    @Override
-    public double getSpeed() {
-		return 268;
-    }
-}
